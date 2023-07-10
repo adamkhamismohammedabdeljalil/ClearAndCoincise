f8a41b20a8a789f3d9fcdfff15a7019db9af4e35
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-10 13:19:22 +0200

    Add ClearAndCoincise project

---
+++ ClearAndCoincise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+var random = new Random();
+
+var range = Enumerable.Range(0, 1000).Select(number => random.Next(0, 2000)).ToList();
+
+Console.WriteLine("Uneven numbers");
+
+printAll(GetUnEvenNumbersMethod(range));
+void printAll(IEnumerable<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        Console.WriteLine(number + " ");
+    }
+    Console.WriteLine();
+}
+foreach (var item in range)
+{
+    Console.WriteLine(item);
+}
+
+//Method syntax
+IEnumerable<int> GetUnEvenNumbersMethod(IEnumerable<int> numbers)
+{
+    //return (from number in numbers
+    //        where number % 2 == 1
+    //        select number);
+   return numbers.Where(number=>number%2==1).Select(number=>number);
+
+}
+
